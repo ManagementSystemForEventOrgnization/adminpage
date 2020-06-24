cb091ad9de0723f1760ba75d74c9bf1d5aa10391
Sang Nguyen <vansangnguyen1998@gmail.com>
2020-06-24 20:21:43 +0700

    noti

--- config/key.js
+++ config/key.js
@@ -2,6 +2,6 @@ module.exports = {
     back_end_uri: `https://event-orgnization.herokuapp.com/api`,
     mongoURI: process.env.MONGOLAB_URI || `mongodb+srv://admin:datn.qlsk.2020@cluster0-9m5xy.mongodb.net/test`,//`mongodb+srv://db_auth_google:sang517119@cluster0-vob0i.mongodb.net/db_test?retryWrites=true&w=majority`,//mongodb+srv://admin:admin@cluster0-9m5xy.mongodb.net/test
     cookieKey:'asdasdqwewqxxzcvcvfgadgaafds',
-    push_key: '3478cd57-df4b-4b16-b5ec-b9ec6545fef1',// '31b8a0fd-e7e8-41a9-b1bc-50428eb4f48e',
-    push_rest_key: 'NDcyYzRhZGUtODJmYy00ZTEyLWI4NjgtYWY3ZjhlZGRlNzEx',// 'ODczNmRmYWMtYjlkNS00M2IwLWE3NmEtMDIwODc1YzllZDBj'
+    push_key: '31b8a0fd-e7e8-41a9-b1bc-50428eb4f48e',// '3478cd57-df4b-4b16-b5ec-b9ec6545fef1',// '31b8a0fd-e7e8-41a9-b1bc-50428eb4f48e',
+    push_rest_key: 'ODczNmRmYWMtYjlkNS00M2IwLWE3NmEtMDIwODc1YzllZDBj',// 'NDcyYzRhZGUtODJmYy00ZTEyLWI4NjgtYWY3ZjhlZGRlNzEx',// 'ODczNmRmYWMtYjlkNS00M2IwLWE3NmEtMDIwODc1YzllZDBj'
 }
